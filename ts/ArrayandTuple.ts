260dddab8b1a98e20c52f992876af9b1d3d4e064
yogyy <yogimr261@gmail.com>
2023-01-17 23:27:26 +0700

    Array and Tuple

---
+++ ts/ArrayandTuple.ts
@@ -0,0 +1,14 @@
+// Array
+let absen: number[] = [1, 2, 3, 4, 5];
+let biodata: any[] = ['yogi', 20, '20', true];
+
+// Tuple
+let biodata2: [number, string, boolean] = [2, 'Tegal', true];
+
+// Tuple Array
+let karyawan: [number, string][];
+karyawan = [
+  [1, 'luthor'],
+  [2, 'john'],
+  [3, 'bruce'],
+];
