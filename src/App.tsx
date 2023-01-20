6a00679eb92931db618f67f42d64bce706a80778
yogyy <yogimr261@gmail.com>
2023-01-20 22:31:15 +0700

    useReducer Strict Action Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
+import Counter from "./components/State/Counter";
 import Loggedin from "./components/State/Loggedin";
 import User from "./components/State/User";
 
 function App() {
   return (
     <div className="app">
-      <Loggedin />
-      <User />
+      <Counter />
     </div>
   );
 }
